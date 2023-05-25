3a92e482e0ca165cb00fb7ce0234cadf5377d5c4
hamishwp <hamishpatten@hotmail.co.uk>
2023-05-25 15:31:14 +0200

    minor mods

--- RCode/MainlyImpactData/GetDesinventar.R
+++ RCode/MainlyImpactData/GetDesinventar.R
@@ -1,5 +1,7 @@
 source("./RCode/Setup/GetPackages.R")
 
+dir.create("./RawData/tmp",showWarnings = F,recursive = T); dir.create("./CleanedData",showWarnings = F,recursive = T)
+
 DesIsos<-c("com", "dji", "eth", "brb", "gmb", "gin", "ken", "mdg", "mli", "mus",
            "moz", "mar", "nam", "ner", "sen", "sle",
            "syc", "tgo", "tun", "uga", "znz", "arg", "blz", "bol", "chl",
