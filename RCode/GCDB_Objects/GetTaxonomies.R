8133a27b0d4a1360aa2bc944ccb650dbc478a557
Hamish Patten <hamishpatten@hotmail.co.uk>
2024-02-21 17:12:07 +0100

    minor mod

--- RCode/GCDB_Objects/GetTaxonomies.R
+++ RCode/GCDB_Objects/GetTaxonomies.R
@@ -3,7 +3,7 @@
 # 
 # 
 # options(gargle_verbosity = "debug")
-# drive_auth(email = "hamish.patten@gmail.com")
+# drive_auth(email = "hamish.patten at gmail.com")
 # 
 # 
 # tmp<-googledrive::drive_download("https://docs.google.com/spreadsheets/d/1agqy6DV5VmJuaamVaXZE7jfkDOC5AOhM/edit?usp=sharing&ouid=109118346520870360454&rtpof=true&sd=true",overwrite = T)
