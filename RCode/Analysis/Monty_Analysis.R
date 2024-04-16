2b2e2908a01f157a289864f35856fdc084bb38f1
hamishwp <hamishpatten@hotmail.co.uk>
2024-04-16 17:25:52 +0200

    Modifying mostly the exceedance curve estimation data + plots

--- RCode/Analysis/Monty_Analysis.R
+++ RCode/Analysis/Monty_Analysis.R
@@ -29,7 +29,8 @@ procDBfore<-function(Monty){
   )
   imp_cols<-c(
     "event_ID","imp_sub_ID","exp_spec_lab","exp_subcat_lab","exp_cat_lab",
-    "imp_value","imp_type_lab","imp_unit_lab","imp_sdate","imp_fdate","imp_ISO3",
+    "imp_value","imp_type_code","imp_unit_code","imp_type_lab",
+    "imp_unit_lab","imp_sdate","imp_fdate","imp_ISO3",
     "imp_unregion","imp_worldbankregion","imp_continent","imp_unsubregion",
     "imp_worldbankincomegroup","imp_spat_covlab","imp_srcdb_code"
   )
@@ -101,6 +102,7 @@ p<-impies%>%filter(haz_Ab%in%c("FL","ST","TC") & worldbankregion=="East Asia & P
   mutate(haz_Ab=factor(haz_Ab,labels=c("Flood","Storm","Tropical Cyclone")))%>%
   ggplot(aes(Month,med,group=haz_Ab))+geom_smooth(aes(ymin=Q05,ymax=Q95,fill=haz_Ab,colour=haz_Ab),alpha=0.3)+
   # geom_ribbon(aes(ymin=Q05,ymax=Q95,fill=haz_Ab),colour="black",alpha=0.3)+
+  coord_cartesian(ylim = c(0, NA))+
   facet_wrap(~haz_Ab) + ylab("Median Deaths") + labs(fill="Hazard",colour="Hazard") ; p
 ggsave("./Plots/HazardSeasonality_Deaths_EastAsiaPacific.png",p,height=5,width=12)
 
@@ -114,8 +116,263 @@ ggsave("./Plots/HazardSeasonality_Events_EastAsiaPacific.png",p,height=5,width=1
 
 
 
+#####################################################################
+######################### EXCEEDANCE CURVES #########################
+#####################################################################
+
+#@@@@@@@@@@@@@@@@@ DEATHS @@@@@@@@@@@@@@@@@#
+
+hazcov<-Monty%>%group_by(haz_Ab)%>%
+  reframe(coverage=as.numeric(max(year) - min(year)))
+
+# haz_Ab=="FL"
+# isos<-c("IDN","PHL","AFG","BGD","COL","KEN","MEX")
+# haz_Ab=="TC"
+# isos<-c("PHL","MEX","VNM","JPN","BGD","MDG","HTI")
+# haz_Ab=="EQ"
+# isos<-c("IDN","IRN","AFG","TUR","JPN","PHL","PER","MEX")
+
+isos=c("PHL","MEX","USA","BGD")
+
+exceed<-Monty%>%filter(imp_type_lab=="Deaths" & imp_srcdb_code=="EM-DAT" &
+                         year>1990 &
+                         !is.na(imp_worldbankregion) & imp_worldbankregion!="Not Classified" &
+                         haz_Ab%in%c("FL","EQ","TC") &
+                         imp_ISO3%in%isos)%>%
+  group_by(haz_Ab,imp_ISO3)%>%
+  reframe(coverage=unique(hazcov$coverage[hazcov==unique(haz_Ab)]),
+          N=n(),
+          impact=sort(imp_value),
+          AAL=mean(impact),
+          ranking=1:N,
+          probability=(1-1/(n():1))/unique(coverage))
+
+p<-exceed%>%ggplot(aes(impact,probability,colour=haz_Ab))+
+  # geom_smooth(se=F,span=0.6)+
+  geom_point(size=2)+geom_line(linewidth=0.3)+
+  xlab("Total Deaths")+ylab("Probability of Occurrence")+
+  ggtitle("EM-DAT Deaths Since 1990")+
+  theme(plot.title = element_text(hjust = 0.5,face="bold",size=14))+
+  labs(colour="Hazard")+
+  scale_y_log10()+
+  scale_x_log10(limits=c(1,10000),labels = scales::label_comma())+
+  facet_wrap(~imp_ISO3); p
+ggsave("./Plots/Example_ExceedanceCurve_EMDAT-Deaths.png",p,height=6,width=10)
+# 
+
+cricov<-Monty%>%filter(imp_ISO3=="CRI")%>%group_by(haz_Ab)%>%
+  reframe(coverage=as.numeric(max(year) - min(year)))
+
+CRI<-Monty%>%filter(imp_type_lab=="Deaths" & imp_srcdb_code=="EM-DAT" &
+                         year>1990 &
+                         haz_Ab%in%c("FL","EQ","TC") &
+                         imp_ISO3%in%c("CRI"))%>%
+  group_by(haz_Ab)%>%
+  reframe(coverage=unique(cricov$coverage[cricov==unique(haz_Ab)]),
+          N=length(imp_value),
+          impact=sort(imp_value),
+          probability=(1-1/(n():1))/unique(coverage))
+
+p<-CRI%>%ggplot(aes(impact,probability,colour=haz_Ab))+
+  # geom_smooth(se=F,span=0.6)+
+  geom_point(size=2)+geom_line(linewidth=0.3)+
+  xlab("Total Deaths")+ylab("Probability of Occurrence")+
+  ggtitle("EM-DAT Deaths Since 1990")+
+  theme(plot.title = element_text(hjust = 0.5,face="bold",size=14))+
+  coord_cartesian(xlim = c(1, NA))+labs(colour="Hazard"); p
+ggsave("./Plots/CRI_ExceedanceCurve_EMDAT-Deaths.png",p,height=6,width=10)
+
+View(CRI)
+
+
+
+#@@@@@@@@@@@@@@@@@ DISPLACEMENT @@@@@@@@@@@@@@@@@#
+
+exceed<-Monty%>%filter(imp_type_lab=="Internally Displaced Persons (IDPs)" & imp_srcdb_code=="GIDD" &
+                         year>2016 &
+                         !is.na(imp_worldbankregion) & imp_worldbankregion!="Not Classified" &
+                         haz_Ab%in%c("FL","EQ","TC") &
+                         imp_ISO3%in%isos)%>%
+  group_by(haz_Ab,imp_ISO3)%>%
+  reframe(coverage=unique(hazcov$coverage[hazcov==unique(haz_Ab)]),
+          N=length(imp_value),
+          impact=sort(imp_value),
+          probability=(1-1/(n():1))/unique(coverage))
+View(exceed)
+
+p<-exceed%>%ggplot(aes(impact,probability,colour=haz_Ab))+
+  # geom_smooth(se=F,span=0.6)+
+  geom_point(size=2)+geom_line(linewidth=0.3)+
+  xlab("Internally Displaced Persons (IDPs)")+ylab("Probability of Occurrence")+
+  ggtitle("GIDD IDPs Since 2016")+
+  theme(plot.title = element_text(hjust = 0.5,face="bold",size=14))+
+  coord_cartesian(xlim = c(10, NA))+labs(colour="Hazard")+
+  scale_y_log10()+scale_x_log10(labels = scales::label_comma())+
+  facet_wrap(~imp_ISO3); p
+ggsave("./Plots/Example_ExceedanceCurve_GIDD-IDPs.png",p,height=6,width=10)
 
 
 
 
+impies<-Monty%>%filter()
 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#%%%%%%%%%%%%%%%%%%%%%%%%% DESINVENTAR SUB-NATIONAL DATA %%%%%%%%%%%%%%%%%%%%%%%%%#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+  
+sapply(seq_along(centrams),function(i){
+  
+  print(centrams[i])
+  
+  iso3<-centrams[i]
+  
+  cntimps<-impies%>%filter(imp_src_db=="Desinventar" & ISO3==iso3)
+  
+  filer<-paste0("./CleanedData/SocioPoliticalData/Desinventar/",
+                stringr::str_to_lower(iso3),
+                "/ADM_",stringr::str_to_lower(iso3),
+                ".geojson")
+  
+  if(!file.exists(filer)) return(F)
+  
+  ADM<-geojsonio::geojson_read(filer, what = "sp")
+  
+  if(iso3%in%c("NPL","LKA")){
+    ADM<-ADM[nchar(ADM$ADMcode)==9,]
+  } else if(iso3=="PAK"){
+    ADM<-ADM[nchar(ADM$ADMcode)==3,]
+  } 
+  
+  ADM$Allrecords<-sapply(ADM$ADMcode,function(codie){
+    sum(grepl(codie,cntimps$imp_spat_ID,ignore.case = T))
+  })
+  
+  ADM$deathsRP<-sapply(ADM$ADMcode,function(codie){
+    output<-tryCatch(cntimps%>%filter(imp_det=="impdetallpeop" & imp_type=="imptypdeat" &
+                                        ev_sdate>1975 & !is.na(imp_value) &
+                                        grepl(codie,imp_spat_ID,ignore.case = T))%>%
+                       impRP_calc(),error=function(e) data.frame(impRP=NA,N=NA))
+    ifelse(output$N>30,output$impRP,NA)
+  })
+  
+  ADM$costRP<-sapply(ADM$ADMcode,function(codie){
+    output<-tryCatch(cntimps%>%filter(imp_subcats%in%c("impecotot","impecodirtot") & imp_type=="imptypcost" & 
+                                        ev_sdate>1975 & !is.na(imp_value) &
+                                        grepl(codie,imp_spat_ID,ignore.case = T))%>%
+                       impRP_calc(),error=function(e) data.frame(impRP=NA,N=NA))
+    ifelse(output$N>30,output$impRP/1e6,NA)
+  })  
+  
+  # Extract the bounding box of the admin boundaries
+  # bbox<-expandBbox(unlist(unname(ExtractBBOXpoly(ADM)[1,2:5])),3)
+  
+  mad_map <- get_stamenmap(expandBbox(ADM@bbox,3),source = "stamen",maptype = "terrain",zoom=5)
+  
+  p<-ggmap(mad_map) + xlab("Longitude") + ylab("Latitude")
+  
+  q<-ADM[ADM$ADMlevel==min(max(ADM$ADMlevel),2),]%>%st_as_sf()%>%ggplot()+
+    geom_sf(aes(fill=Allrecords), color = "grey30", linewidth=0.1)+ #, inherit.aes = FALSE) +
+    scale_fill_gradient("No. Records",low="magenta4", high="magenta", trans = "log10",na.value = "black");q #, inherit.aes = FALSE) +
+  ggsave(paste0("Allrecords_ADM2_",iso3,"_Dessie.png"),q,path="./Plots/GCDB_Workshop/Sub-national/",width = 10)  
+  
+  q<-ADM[ADM$ADMlevel==min(ADM$ADMlevel),]%>%st_as_sf()%>%ggplot()+
+    geom_sf(aes(fill=Allrecords), color = "grey30", linewidth=0.1)+ #, inherit.aes = FALSE) +
+    scale_fill_gradient("No. Records",low="magenta4", high="magenta", trans = "log10",na.value = "black");q #, inherit.aes = FALSE) +
+  ggsave(paste0("Allrecords_ADM1_",iso3,"_Dessie.png"),q,path="./Plots/GCDB_Workshop/Sub-national/",width = 10)  
+  
+  q<-ADM[ADM$ADMlevel==min(ADM$ADMlevel),]%>%st_as_sf()%>%ggplot()+
+    geom_sf(aes(fill=deathsRP), color = "grey30", linewidth=0.1, inherit.aes = FALSE) +
+    scale_fill_gradient("Exp. No. Deaths 5Yr RP",low="magenta4", high="magenta", trans = "log10",na.value = "black");q #, inherit.aes = FALSE) +
+  ggsave(paste0("DeathsRP_Allrecords_ADM1_",iso3,"_Dessie.png"),q,path="./Plots/GCDB_Workshop/Sub-national/",width = 10)  
+  
+  q<-ADM[ADM$ADMlevel==min(ADM$ADMlevel),]%>%st_as_sf()%>%ggplot()+
+    geom_sf(aes(fill=costRP), color = "grey30", linewidth=0.1, inherit.aes = FALSE) +
+    scale_fill_gradient("Exp. Cost 5Yr RP [Millions Local Curr]",high="chartreuse",low="chartreuse4",trans = "log10",na.value = "black");q #, inherit.aes = FALSE) +
+  ggsave(paste0("CostRP_Allrecords_ADM1_",iso3,"_Dessie.png"),q,path="./Plots/GCDB_Workshop/Sub-national/",width = 10)  
+  
+  sapply(lhaz,function(hazzie){
+    
+    ADM$records<-sapply(ADM$ADMcode,function(codie){
+      sum(grepl(codie,cntimps$imp_spat_ID[cntimps$haz_Ab==hazzie],ignore.case = T))
+    })
+    
+    q<-ADM[ADM$ADMlevel==min(max(ADM$ADMlevel),2),]%>%st_as_sf()%>%ggplot()+
+      geom_sf(aes(fill=records), color = "grey30", linewidth=0.1)+
+      scale_fill_gradient(paste0("No. Records - ",hazzie), high=pal[names(pal)==hazzie], trans = "log10",na.value = "black");q #, inherit.aes = FALSE) +
+    ggsave(paste0(hazzie,"_records_ADM2_",iso3,"_Dessie.png"),q,path="./Plots/GCDB_Workshop/Sub-national/",width = 10)  
+    
+    q<-ADM[ADM$ADMlevel==min(ADM$ADMlevel),]%>%st_as_sf()%>%ggplot()+
+      geom_sf(aes(fill=records), color = "grey30", linewidth=0.1)+
+      scale_fill_gradient(paste0("No. Records - ",hazzie),high=pal[names(pal)==hazzie], trans = "log10",na.value = "black");q #, inherit.aes = FALSE) +
+    ggsave(paste0(hazzie,"_records_ADM1_",iso3,"_Dessie.png"),q,path="./Plots/GCDB_Workshop/Sub-national/",width = 10)  
+    
+    return(T)},simplify = T)
+  
+  return(T)},simplify = T)
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%% EM-DAT SUB-NATIONAL DATA %%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+ADM<-GAUL2Monty(iso3,forcer=T)
+
+sapply(seq_along(centrams),function(i){
+  
+  iso3<-centrams[i]
+  
+  cntimps<-Monty%>%filter(imp_srcdb_code=="EM-DAT" & imp_ISO3==iso3)
+  
+  filer<-paste0("./CleanedData/SocioPoliticalData/EMDAT/",
+                iso3,"/ADM_",iso3,
+                ".geojson")
+  
+  if(!file.exists(filer)) return(F)
+  
+  ADM<-geojsonio::geojson_read(filer, what = "sp")
+  
+  ADM1<-aggregate(ADM, by = "ADM1_CODE")
+  ADM2<-aggregate(ADM, by = "ADM2_CODE")
+  
+  ADM2$Allrecords<-sapply(ADM2$ADM2_CODE,function(codie){
+    sum(grepl(codie,cntimps$imp_spat_ID,ignore.case = T))
+  })
+  
+  q<-ADM2%>%st_as_sf()%>%ggplot()+
+    geom_sf(aes(fill=Allrecords), color = "grey30", linewidth=0.1)+ #, inherit.aes = FALSE) +
+    scale_fill_gradient("No. Records",low="magenta4", high="magenta", trans = "log10",na.value = "black");q #, inherit.aes = FALSE) +
+  ggsave(paste0("Allrecords_ADM2_",iso3,"_EMDAT.png"),q,path="./Plots/GCDB_Workshop/Sub-national/",width = 10)  
+  
+  ADM1$Allrecords<-sapply(ADM1$ADM1_CODE,function(codie){
+    sum(grepl(codie,cntimps$imp_spat_ID,ignore.case = T))
+  })
+  
+  q<-ADM1%>%st_as_sf()%>%ggplot()+
+    geom_sf(aes(fill=Allrecords), color = "grey30", linewidth=0.1)+ #, inherit.aes = FALSE) +
+    scale_fill_gradient("No. Records",low="magenta4", high="magenta", trans = "log10",na.value = "black");q #, inherit.aes = FALSE) +
+  ggsave(paste0("Allrecords_ADM1_",iso3,"_EMDAT.png"),q,path="./Plots/GCDB_Workshop/Sub-national/",width = 10)  
+  
+  
+  sapply(lhaz,function(hazzie){
+    
+    ADM2$records<-sapply(ADM2$ADM2_CODE,function(codie){
+      sum(grepl(codie,cntimps$imp_spat_ID[cntimps$haz_Ab==hazzie],ignore.case = T))
+    })
+    
+    q<-ADM2%>%st_as_sf()%>%ggplot()+
+      geom_sf(aes(fill=records), color = "grey30", linewidth=0.1)+ #, inherit.aes = FALSE) +
+      scale_fill_gradient(paste0("No. Records - ",hazzie),high=pal[names(pal)==hazzie], trans = "log10",na.value = "black");q #, inherit.aes = FALSE) +
+    ggsave(paste0(hazzie,"_records_ADM2_",iso3,"_EMDAT.png"),q,path="./Plots/GCDB_Workshop/Sub-national/",width = 10)  
+    
+    ADM1$records<-sapply(ADM1$ADM1_CODE,function(codie){
+      sum(grepl(codie,cntimps$imp_spat_ID[cntimps$haz_Ab==hazzie],ignore.case = T))
+    })
+    
+    q<-ADM1%>%st_as_sf()%>%ggplot()+
+      geom_sf(aes(fill=records), color = "grey30", linewidth=0.1)+ #, inherit.aes = FALSE) +
+      scale_fill_gradient(paste0("No. Records - ",hazzie),high=pal[names(pal)==hazzie], trans = "log10",na.value = "black");q #, inherit.aes = FALSE) +
+    ggsave(paste0(hazzie,"_records_ADM1_",iso3,"_EMDAT.png"),q,path="./Plots/GCDB_Workshop/Sub-national/",width = 10)  
+    
+    return(T)},simplify = T)
+  
+  return(T)},simplify = T)
