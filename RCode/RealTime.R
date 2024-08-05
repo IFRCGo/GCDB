c85e5762058cc22a8dba0d3cf03260f5bcc7c76d
hamishwp <hamishpatten@hotmail.co.uk>
2024-08-05 11:49:01 +0200

    Starting coding up the real-time ETL scripts

---
+++ RCode/RealTime.R
@@ -0,0 +1,64 @@
+# Read in all the necessary libraries and GCDB scripts
+source("./RCode/Setup/GetPackages.R")
+
+# Daily Monty
+DailyMontyEDL<-function(){
+  # IFRC DREF-EA data
+  Monty<-checkMonty(convGOApp_Monty())
+  # EM-DAT
+  Monty<-tryCatch(MergeMonty(list(Monty,checkMonty(convEMDAT_Monty()))),
+                  error=function(e) {print("Error extracting CRED - EM-DAT data"); return(Monty)})
+  # IDMC GIDD
+  Monty<-tryCatch(MergeMonty(list(Monty,checkMonty(convGIDD_Monty()))),
+                  error=function(e) {print("Error extracting IDMC - GIDD data"); return(Monty)})
+  # IDMC IDU
+  Monty<-tryCatch(MergeMonty(list(Monty,checkMonty(convIDU_Monty()))),
+                  error=function(e) {print("Error extracting IDMC - IDU data"); return(Monty)})
+  # GLIDE
+  Monty<-tryCatch(MergeMonty(list(Monty,checkMonty(convGLIDE_Monty()))),
+                  error=function(e) {print("Error extracting ADRC - GLIDE data"); return(Monty)})
+  # Desinventar
+  Monty<-tryCatch(MergeMonty(list(Monty,checkMonty(convDessie_Monty()))),
+                  error=function(e) {print("Error extracting UNDRR - Desinventar data"); return(Monty)})
+  # DFO
+  Monty<-tryCatch(MergeMonty(list(Monty,checkMonty(convDFO_Monty()))),
+                  error=function(e) {print("Error extracting University Columbia - DFO data"); return(Monty)})
+  
+  return(Monty)
+}
+
+# 10-minute Monty
+MinuteMontyEDL<-function(){
+  # GDACS
+  Monty<-tryCatch(MergeMonty(list(Monty,checkMonty(convGDACS_Monty()))),
+                  error=function(e) {print("Error extracting EC-JRC - GDACS data"); return(Monty)})
+  # PDC Forecasts
+  
+  # ADAM Forecasts
+  
+  # USGS Atlas Shakemaps
+  
+  
+  return(Monty)
+}
+
+# Difference Monty
+DiffRealTimeMonty<-function(Monty){
+  # Get the Monty data to compare to
+  cMonty<-GetCurrMonty()
+  # 
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
